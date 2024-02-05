396ce242ec9044702bb8e87f2bcfcfc24b5a19d3
Hila Dolev <hiladolev1@gmail.com>
2024-02-05 16:37:31 +0200

    changed the route name from "controller" to the name of the controller

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace DotnetAPI.Controllers;
 
 [ApiController]
-[Route("controller")]
+[Route("[controller]")]
 
 public class WeatherForecastController : ControllerBase
 {
