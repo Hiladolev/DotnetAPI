e8bb0f27c5850b624189575cdeca478c6472b95e
Hila Dolev <hiladolev1@gmail.com>
2024-02-12 15:08:59 +0200

    function that execute a sql query and returns a bool for whether the execute worked

--- Data/DataContextDapper.cs
+++ Data/DataContextDapper.cs
@@ -23,5 +23,10 @@ namespace DotnetAPI
             IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
             return dbConnection.QuerySingle<T>(sql);
         }
+        public bool ExecuteSql(string sql)
+        {
+            IDbConnection dbConnection = new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+            return dbConnection.Execute(sql)>0;
+        }
     }
 }
