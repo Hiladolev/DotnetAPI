615cea167c146fccbd7c0f205320600954dc34cb
Hila Dolev <hiladolev1@gmail.com>
2024-02-24 15:38:54 +0200

    fixed misspell in the password hash field

--- Dtos/UserForLoginConfirmationDto.cs
+++ Dtos/UserForLoginConfirmationDto.cs
@@ -2,7 +2,7 @@ namespace DotnetAPI.Dtos
 {
     public partial class UserForLoginConfirmationDto
     {
-        public byte[] PassworkdHash {get;set;} = new byte[0];
+        public byte[] PasswordHash {get;set;} = new byte[0];
         public byte[] PasswordSalt {get;set;} = new byte[0];
     }
 
