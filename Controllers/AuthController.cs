a2c62fe60f0058da8be764356dd0f92d57831c0b
Hila Dolev <hiladolev1@gmail.com>
2024-02-24 14:19:21 +0200

    register method - validating if the password and password confirm equal + if the email already exist + converting the password to password salt and password hash and inserting it to the db

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,5 +1,11 @@
+using System.Data;
+using System.Security.Cryptography;
+using System.Text;
 using DotnetAPI.Data;
+using DotnetAPI.Dtos;
+using Microsoft.AspNetCore.Cryptography.KeyDerivation;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
 
 namespace DotnetAPI
 {
@@ -11,6 +17,51 @@ namespace DotnetAPI
         {
             _dapper = new DataContextDapper(config);
             _config = config;
+        }
+        [HttpPost("Register")]
+        public IActionResult Register(UserForRegistrationDto userForRegistration)
+        {
+            if(userForRegistration.Password == userForRegistration.PasswordConfirm)
+            {
+                string sqlCheckUserExist = @"SELECT Email FROM TutorialAppSchema.Auth WHERE Email= '" + userForRegistration.Email + "'";
+            if(_dapper.ExecuteSql(sqlCheckUserExist))throw new Exception("Email already exist");
+           byte[] passwordSalt = new byte[128 / 8];
+           using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+           {
+            rng.GetNonZeroBytes(passwordSalt);
+           }
+           string passwordSaltPlusString = _config.GetSection("AppSettings:PasswordKey").Value + Convert.ToBase64String(passwordSalt);
+           byte[] passwordHash = KeyDerivation.Pbkdf2(
+            password: userForRegistration.Password,
+            salt: Encoding.ASCII.GetBytes(passwordSaltPlusString),
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8
+           );
+            string sqlAddAuth = @"INSERT INTO TutorialAppSchema.Auth
+            ([Email],
+            [PasswordHash],
+            [PasswordSalt]
+            ) VALUES (
+                '" + userForRegistration.Email +
+                "', @PaaswordHash, @PasswordSalt)";
+           List<SqlParameter> sqlParameters = new List<SqlParameter>();
+           SqlParameter passwordSaltParameter = new SqlParameter("@PasswordSalt",SqlDbType.VarBinary);
+                passwordSaltParameter.Value = passwordSalt;
+           SqlParameter passwordHashParameter = new SqlParameter("@PasswordHash",SqlDbType.VarBinary);
+                passwordHashParameter.Value = passwordHash;
+            sqlParameters.Add(passwordSaltParameter);
+            sqlParameters.Add(passwordHashParameter);
+                if(_dapper.ExecuteSqlWithParameters(sqlAddAuth, sqlParameters)) return Ok();
+                throw new Exception("Failed to register user");
+            }
+                throw new Exception("User password and password confirmation don't match");
+
+        }
+        [HttpPost("Login")]
+        public IActionResult Login(UserForLoginDto userForLogin)
+        {
+            return Ok();
         }
     }
 }
