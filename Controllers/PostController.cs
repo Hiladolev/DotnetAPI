5edf2156c4a49b906b1ff05dc05f5ac356d8e4c3
Hila Dolev <hiladolev1@gmail.com>
2024-02-25 15:05:08 +0200

    post controller initialization + enable access to dapper

---
+++ Controllers/PostController.cs
@@ -0,0 +1,19 @@
+using DotnetAPI.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DotnetAPI.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class PostController:ControllerBase
+    {
+        private readonly DataContextDapper _dapper;
+        public PostController(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+        }
+
+    } 
+}
