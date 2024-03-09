ca6fa717be651e4b9b35ad223d248d2d74c29996
Hila Dolev <hiladolev1@gmail.com>
2024-03-09 14:55:22 +0200

    added a new method to make the set up of user password hash and salt to be reusable

--- Helpers/AuthHelper.cs
+++ Helpers/AuthHelper.cs
@@ -1,7 +1,12 @@
+using System.Data;
 using System.IdentityModel.Tokens.Jwt;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
+using DotnetAPI.Data;
+using DotnetAPI.Dtos;
 using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Microsoft.Data.SqlClient;
 using Microsoft.IdentityModel.Tokens;
 
 namespace DotnetAPI.Helpers
@@ -9,9 +14,11 @@ namespace DotnetAPI.Helpers
     public class AuthHelper
     {
         private readonly IConfiguration _config;
+        private readonly DataContextDapper _dapper;
 
         public AuthHelper(IConfiguration config)
         {
+            _dapper = new DataContextDapper(config);
             _config = config;
         }
         public byte[] GetPasswordHash(string password, byte[] passwordSalt)
@@ -49,6 +56,39 @@ namespace DotnetAPI.Helpers
             SecurityToken token = tokenHandler.CreateToken(descriptor);
         
             return tokenHandler.WriteToken(token);
+        }
+        public bool SetPassword(UserForLoginDto userForSetPassword)
+        {
+                       byte[] passwordSalt = new byte[128 / 8];
+           using (RandomNumberGenerator rng = RandomNumberGenerator.Create())
+           {
+            rng.GetNonZeroBytes(passwordSalt);
+           }
+          byte[] passwordHash = GetPasswordHash(userForSetPassword.Password,passwordSalt);
+            string sqlAddAuth = @"EXEC TutorialAppSchema.spRegistration_Upsert
+                @Email = @EmailParam,
+                @PasswordHash = @PasswordHashParam, 
+                @PasswordSalt = @PasswordSaltParam";
+
+           List<SqlParameter> sqlParameters = [];
+
+                SqlParameter emailParameter = new("@EmailParam", SqlDbType.VarChar)
+                {
+                    Value = userForSetPassword.Email
+                };
+                sqlParameters.Add(emailParameter);
+                SqlParameter passwordSaltParameter = new("@PasswordSaltParam", SqlDbType.VarBinary)
+                {
+                    Value = passwordSalt
+                };
+                sqlParameters.Add(passwordSaltParameter);
+
+                SqlParameter passwordHashParameter = new("@PasswordHashParam", SqlDbType.VarBinary)
+                {
+                    Value = passwordHash
+                };
+                sqlParameters.Add(passwordHashParameter);
+                return _dapper.ExecuteSqlWithParameters(sqlAddAuth, sqlParameters);
         }
     }
 }
