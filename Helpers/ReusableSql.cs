0a52839e278c025b0c0d518068b0f273881c2d67
Hila Dolev <hiladolev1@gmail.com>
2024-03-11 18:01:45 +0200

    having a filed of dapper to use it inside the file

--- Helpers/ReusableSql.cs
+++ Helpers/ReusableSql.cs
@@ -1,7 +1,15 @@
+using DotnetAPI.Data;
+
 namespace DotnetAPI.Helpers
 {
     public class ReusableSql
     {
-        
+        private readonly DataContextDapper _dapper;
+
+        public ReusableSql(IConfiguration config)
+        {
+            _dapper = new DataContextDapper(config);
+        }
+    
     }
 }
